46727be03ef5c6220dbefd36fc197879b25591ef
Frédrick Jeannot <e2474531@cmaisonneuve.qc.ca>
2025-12-12 21:45:23 -0500

    Modification du serveur

--- serveur/app.js
+++ serveur/app.js
@@ -8,9 +8,14 @@ app.use(express.json());
 //servir les fichiers du dossier CLIENT
 app.use(express.static(path.join(__dirname, "../client")));
 
-//=======================
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+
+
 //api photos picsum
-//=======================
+
 
 //liste de photos depuis l'API Picsum
 app.get("/api/photos", async (req, res)=>{
